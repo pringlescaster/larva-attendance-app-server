9b3bee1dd94c5a33287262e881f65f3365205279
DAVID <pringlescaster@gmail.com>
2024-10-14 12:17:33 +0100

    updated app.js

--- app.js
+++ app.js
@@ -34,10 +34,7 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 2000;
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
